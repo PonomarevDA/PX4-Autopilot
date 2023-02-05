bdfa2947a28ebc535f6844523fadbe6044d13ae4
Dmitry Ponomarev <ponomarevda96@gmail.com>
2023-02-06 00:55:52 +0300

    Cyphal: add port.List

--- src/drivers/cyphal/SubscriptionManager.hpp
+++ src/drivers/cyphal/SubscriptionManager.hpp
@@ -96,6 +96,7 @@
 
 #include <px4_platform_common/defines.h>
 #include <drivers/drv_hrt.h>
+#include <uavcan/node/port/List_0_1.h>
 #include "Subscribers/DynamicPortSubscriber.hpp"
 #include "CanardInterface.hpp"
 
@@ -130,6 +131,7 @@ public:
 	void subscribe();
 	void printInfo();
 	void updateParams();
+	void fillSubjectIdList(uavcan_node_port_SubjectIDList_0_1 &subscribers_list);
 
 private:
 	void updateDynamicSubscriptions();
